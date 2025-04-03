75f6d66a41d49dc7531eb74d9d4076bc49044082
Faruk Joldic <faruk.joldic@unravel.team>
2025-04-03 11:31:22 +0200

    Add: Tab Layout (header)
    Created a tab layout in the header with 4 different tabs that a user can click on

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
 }
